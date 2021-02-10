5e95def795993be84c97528e5af18f989b85534a
M ZDANOV <m.zhdanov@itfbgroup.ru>
2021-02-10 14:58:34 +0300

    1st

---
+++ src/test/java/ru/pronnikov/springLearn/SpringLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.pronnikov.springLearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
